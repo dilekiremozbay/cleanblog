9fd29ecaad008175ee62904adcc5d6da3b945bd2
dilekiremozbay <dilekiremozbay@gmail.com>
2022-01-10 00:27:14 +0300

    second homework

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 2000;
